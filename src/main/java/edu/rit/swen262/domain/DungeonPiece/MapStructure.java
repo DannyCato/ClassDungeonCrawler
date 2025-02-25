9f8bb1d86163b120745ee6b158eb40b348e561b1
DannyCato <dg@catorcini.com>
2025-02-25 11:23:23 -0500

    Added a MapStructure and RoomNode class to help define how map contains rooms

--- src/main/java/edu/rit/swen262/domain/DungeonPiece/MapStructure.java
+++ src/main/java/edu/rit/swen262/domain/DungeonPiece/MapStructure.java
@@ -1,8 +1,28 @@
 package edu.rit.swen262.domain.DungeonPiece;
 
+import java.util.HashMap;
+
+import edu.rit.swen262.domain.DirectionalVector;
+
 /**
  * Helper Class to manage the layout of Map
  */
 public class MapStructure {
-    
+    private HashMap<RoomNode, RoomNode> rooms;
+
+    public MapStructure(Room root) {
+        this.rooms = new HashMap<>();
+    }
+
+    public boolean addRoom(RoomNode key, RoomNode value, DirectionalVector dir) {
+        RoomNode room = getRoom(key);
+        if (room == null) {
+            room = rooms.put(key, key);
+        }
+        return room.setConnection(value, dir);
+    }
+
+    public RoomNode getRoom(RoomNode key) {
+        return rooms.get(key);
+    }
 }
--- src/main/java/edu/rit/swen262/domain/DungeonPiece/RoomNode.java
+++ src/main/java/edu/rit/swen262/domain/DungeonPiece/RoomNode.java
@@ -1,9 +1,55 @@
 package edu.rit.swen262.domain.DungeonPiece;
 
-import java.util.ArrayList;
+import edu.rit.swen262.domain.DirectionalVector;
 
 public class RoomNode {
     private Room room;
 
-    private ArrayList<RoomNode> children;
+    private RoomNode north;
+    private RoomNode east;
+    private RoomNode south;
+    private RoomNode west;
+    
+    public RoomNode(Room room) {
+        this.room = room ;   
+    }
+
+    public boolean setConnection(RoomNode r, DirectionalVector dir) {
+        switch (dir) {
+            case DirectionalVector.NORTH:
+                north = r;
+                break;
+            case DirectionalVector.SOUTH:
+                south = r;
+                break;
+            case DirectionalVector.EAST:
+                east = r;
+                break;
+            case DirectionalVector.WEST:
+                west = r;
+                break;
+            default:
+                return false;
+        }
+        return true;
+    }
+        
+    public RoomNode getConnection(Room room, DirectionalVector dir) {
+        switch (dir) {
+            case DirectionalVector.NORTH:
+                return north;
+            case DirectionalVector.SOUTH:
+                return south;
+            case DirectionalVector.EAST:
+                return east;
+            case DirectionalVector.WEST:
+                return west;
+            default:
+                return null;
+        }
+    }
+
+    public Room getRoom() {
+        return room;
+    }
 }
