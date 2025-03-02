dfce6aa766a38cd17d9bd4ea1f274335a9267102
DannyCato <dg@catorcini.com>
2025-03-01 20:03:52 -0500

    pushing changes

--- src/main/java/edu/rit/swen262/service/RoomFiller.java
+++ src/main/java/edu/rit/swen262/service/RoomFiller.java
@@ -9,24 +9,42 @@ import java.util.Queue;
 import java.util.Random;
 
 import edu.rit.swen262.domain.DirectionalVector;
+import edu.rit.swen262.domain.Exit;
 import edu.rit.swen262.domain.DungeonPiece.DungeonPiece;
 import edu.rit.swen262.domain.DungeonPiece.Room;
+import edu.rit.swen262.domain.DungeonPiece.RoomNode;
 import edu.rit.swen262.domain.DungeonPiece.Tile;
 
 public class RoomFiller {
     //this is so broken dude
-    private static final Random rand = new Rando
+        private static final Random rand = new Random();
 
     public static void fill(Room room) {
         Room originalRoom = room; 
         room = new Room(room.width, room.height);
         int size = room.height * room.width;
 
+        ArrayList<RoomNode> rns = originalRoom.getRoomNode().getAllConnections();
         for (int i = 0; i < size; i++) {
-            Tile t = new Tile();
-            if ()
+            double chance = rand.random();
+            if (((i < room.width) || ((i % room.width) == (room.width - 1)) || (i > (room.height - 1) * room.width) || ((i % room.width) == 0))
+            // && !(i == 0 || i == (room.height - 1 ) * room.width || i == width || i == size - 1
+            ) {
+                if (i < room.width) {
+                    
+                }
+                DirectionalVector exitDir = room.tileOnEdge(t);
+                if (room.tileOnEdge(t) == null) {
+                    continue;
+                }
+                if (rns.get(i) == null) {
+                    continue;
+                }
+                Exit exit = new Exit("", exitDir);
+                Tile newT = new Tile(exit);
+                room.addTile();
+            } 
             
-            room.addTile();
         }
     }
 
