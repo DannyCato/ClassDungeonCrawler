6b93c3967f49c3a3701b353a797386d6a28d8c5a
emanning686 <emanning1030@gmail.com>
2025-03-01 20:04:44 -0500

    validate room

--- src/main/java/edu/rit/swen262/service/RoomFiller.java
+++ src/main/java/edu/rit/swen262/service/RoomFiller.java
@@ -52,21 +52,30 @@ public class RoomFiller {
         Tile[] exits = new ArrayList<>(Arrays.asList(DirectionalVector.NORTH, DirectionalVector.EAST, DirectionalVector.SOUTH, DirectionalVector.WEST)).stream().map(dir -> room.getExitTileByDirection(dir)).filter(Objects::nonNull).toArray(Tile[]::new);
 
         for (Tile exit1 : exits) {
-             for (Tile exit2 : exits) {
+            for (Tile exit2 : exits) {
                 if (exit1 != exit2) {
                     Queue<Tile> queue = new PriorityQueue<>();
-                    queue.add(exit);
+                    queue.add(exit1);
                     boolean ExitFound = false;
-                    while (ExitFound == false && queue.size() > 0) {
+                    while (ExitFound == false) {
+                        if (queue.isEmpty()) {
+                            return false;
+                        }
                         Tile current = queue.poll();
-                        if (current == exit) {
+                        if (current == exit2) {
+                            ExitFound = true;
+                        }
                         Collection<DungeonPiece<Tile>> neighbors = new ArrayList<>();
                         room.getAllAdjacentTiles(current, neighbors);
                         for (DungeonPiece<Tile> neighbor : neighbors) {
-                            queue.add((Tile)neighbor);
+                            if (Objects.nonNull(neighbor)) {
+                                queue.add((Tile)neighbor);
+                            }
                         }
                     }
                 }
+            }
         }
+        return true;
     }
 }
