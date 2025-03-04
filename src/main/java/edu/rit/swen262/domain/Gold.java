26f5853543efc897375f3ec1e20b0c73def58089
Nick <nfennelly01@gmail.com>
2025-03-03 23:34:37 -0500

    added gold docstring

--- src/main/java/edu/rit/swen262/domain/Gold.java
+++ src/main/java/edu/rit/swen262/domain/Gold.java
@@ -1,18 +1,39 @@
 package edu.rit.swen262.domain;
 
+/**
+ * Represents Gold, the in game currency of the M.U.D game.
+ * Gold is an item, however it acts as its own standalone object
+ * where its value is determined by the count.
+ * Extends the {@code Item} interface.
+ * 
+ * @author [Nick F, Ryan M]
+ */
+
 public class Gold extends Item {
 
+    /** The quantity of gold the player has. */
     private int count;
-
+    
+    /**
+     * Creates a Gold item given a quantity.
+     * 
+     * @param count
+     */
     public Gold(int count) {
         super("Gold", "This item is a form of currency.", null);
         this.count = count;
     }
 
+    /**
+     * Increases the player's gold by one.
+     */
     public void increaseCount() {
         this.count++;
     }
 
+    /**
+     * Decreases the player's gold by one, if the player has more than 0 pieces.
+     */
     public void decreaseCount() {
         this.count--;
         if (this.count < 0) {
@@ -20,15 +41,27 @@ public class Gold extends Item {
         }
     }
 
+    /**
+     * Gets the value of the gold (Returns itself.)
+     */
     @Override
     public Gold getValue() {
         return this;
     }
 
+    /**
+     * Gets the total count of the gold the user has.
+     */
     public int getCount() {
         return count;
     }
 
+    /**
+     * Returns a clean string representation of a Gold item including the
+     * name, description, and count.
+     * 
+     * @return A string describing the Gold.
+     */
     @Override
     public String toString() {
         return String.format("%s: %s%nValue: %s", getName(), getDescription(), getCount());
