4a81ca767a124dcf03995673b9c4dd34997434e9
Nick <nfennelly01@gmail.com>
2025-03-03 23:20:39 -0500

    armor and bag docstrings created

--- src/main/java/edu/rit/swen262/domain/Armor.java
+++ src/main/java/edu/rit/swen262/domain/Armor.java
@@ -1,18 +1,42 @@
 package edu.rit.swen262.domain;
 
+/**
+ * Represents an armor item in game, increasing the player's defensive stats.
+ * Extends from {@code Item}
+ * 
+ * @author [Nick F, Ryan M]
+ */
+
 public class Armor extends Item {
 
+    /** Defense value for the armor item */
     private int defense;
 
+    /**
+     * 
+     * Creates an Armor item using the specified value
+     * 
+     * @param name The name of the armor
+     * @param description A description of the armor
+     * @param defense A defense value associated with the armor
+     * @param value The value of the armor given in gold
+     */
     public Armor(String name, String description, int defense, Gold value) {
         super(name, description, value);
         this.defense = defense;
     }
-
+    
+    /**
+     * @return Gets the defense value of the armor
+     */
     public int getDefense() {
         return defense;
     }
 
+    /**
+     * Returns a clean string representation of an armor item, using the name, description, defense, and count.
+     * @return
+     */
     @Override
     public String toString() {
         Gold gold = getValue();
--- src/main/java/edu/rit/swen262/domain/Bag.java
+++ src/main/java/edu/rit/swen262/domain/Bag.java
@@ -3,21 +3,51 @@ package edu.rit.swen262.domain;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Represents a bag that can hold items, acts as a container
+ * Has a defined capacity to store a specific amount of items
+ * 
+ * @author [Nick F, Ryan M]
+ */
+
 public class Bag {
 
+    /** 
+     * The items stored in a bag.
+    */
     private List<Item> contents;
+    /**
+     * The number of items a bag can hold.
+     */
     private int capacity;
 
+    /**
+     * Creates a bag with a specific capacity as an empty list.
+     * 
+     * @param capacity The maxmium number of items a bag can hold.
+     */
     public Bag(int capacity) {
         this.contents = new ArrayList<Item>();
         this.capacity = capacity;
     }
 
+    /**
+     * Creates a bag a list of items and specific capacity.
+     * 
+     * @param contents The items that the bag holds.
+     * @param capacity The maximum number of items a bag can hold.
+     */
     public Bag(List<Item> contents, int capacity) {
         this.contents = contents;
         this.capacity = capacity;
     }
 
+    /**
+     * Tries to add an item to a bag if it is not full
+     * 
+     * @param item The item that will try to get added.
+     * @return Returns {@code true} if the item was added, {@code false} if it was not.
+     */
     public boolean addItem(Item item) {
         if (contents.size() < capacity) {
             return contents.add(item);
@@ -26,21 +56,41 @@ public class Bag {
 
     }
 
+    /**
+     * Tries to remove an item from the bag.
+     * 
+     * @param item The item that will be removed.
+     * @return Returns {@code true} if the item was removed, {@code false} if it was not.
+     */
     public boolean removeItem(Item item) {
         return contents.remove(item);
 
     }
 
+    /**
+     * Gets the contents stored in a bag.
+     * @return The list of item objects in a bag.
+     */
     public List<Item> getItems() {
         return contents;
 
     }
 
+    /**
+     * Gets the maxmimum capacity of a bag.
+     * @return The capacity of a bag.
+     */
     public int getCapacity() {
         return capacity;
     }
 
-
+    /**
+     * Compares a bag to another bag for equality.
+     * The two are considered equal if both contain the same items and have the same capacity.
+     * 
+     * @param obj The object that will be compared to.
+     * @return Returns {@code true} if the objects are equal, {@code false} if they are not.
+     */
     @Override
     public boolean equals(Object obj) {
         if (obj instanceof Bag) {
