a8968f4ca01445f7cad1487e0a41bd57c2ee60dc
nickf1130 <nfennelly01@gmail.com>
2025-04-01 13:41:09 -0400

    basic attack system implemented with test

--- src/main/java/edu/rit/swen262/domain/Character.java
+++ src/main/java/edu/rit/swen262/domain/Character.java
@@ -1,77 +1,109 @@
-package edu.rit.swen262.domain;
-
-/**
- * a representation of a Character within the MUD Game, which has a name, decsription
- * and stats, and is capable of attacking any other Character on the Map.
- * 
- * @author Victor Bovat
- */
-public abstract class Character {
-    String name;
-    String description;
-    int health;
-    int maxHP;
-    int attack;
-    int defense;
+    package edu.rit.swen262.domain;
 
     /**
-     * initializes a {@link Character} with the given name, description, 
-     * and max health, attack, and defense stats. by default, the health
-     * stat is set to the full maxHP value.
+     * a representation of a Character within the MUD Game, which has a name, decsription
+     * and stats, and is capable of attacking any other Character on the Map.
      * 
-     * @param name the name of the character
-     * @param description the brief description of the character
-     * @param health the health stat of the character
-     * @param attack the attack stat of the character
-     * @param defense the defense stat of the character
+     * @author Victor Bovat
      */
-    public Character(String name, String description, int maxHP, int attack, 
-    int defense) {
-        this.name = name;
-        this.description = description;
-        this.health = maxHP;
-        this.maxHP = maxHP;
-        this.attack = attack;
-        this.defense = defense;
-    }
+    public abstract class Character {
+        String name;
+        String description;
+        int health;
+        int maxHP;
+        int attack;
+        int defense;
 
-    /**
-     * attacks the other specified {@link Character} and deducts health from
-     * them based upon the initaor's attack stat and the opponent's defense stat.
-     * If the calculated damage is more than the opponent's current health, then
-     * it will instead be set to zero.
-     * 
-     * @param opponent the {@link Character} to be attacked
-     */
-    public void attack(Character opponent) {
-        /* TO-DO: implement one-sided attack from character -> opponent
-        and deduct the correct health amt based upon atk and def */ 
-    }
+        /**
+         * initializes a {@link Character} with the given name, description, 
+         * and max health, attack, and defense stats. by default, the health
+         * stat is set to the full maxHP value.
+         * 
+         * @param name the name of the character
+         * @param description the brief description of the character
+         * @param health the health stat of the character
+         * @param attack the attack stat of the character
+         * @param defense the defense stat of the character
+         */
+        public Character(String name, String description, int maxHP, int attack, 
+        int defense) {
+            this.name = name;
+            this.description = description;
+            this.health = maxHP;
+            this.maxHP = maxHP;
+            this.attack = attack;
+            this.defense = defense;
+        }
 
-    /**
-     * fetches the name of the {@link Character}
-     * 
-     * @return name of the {@link Character}
-     */
-    public String getName() {
-        return this.name;
-    }
+        /**
+         * attacks the other specified {@link Character} and deducts health from
+         * them based upon the initaor's attack stat and the opponent's defense stat.
+         * If the calculated damage is more than the opponent's current health, then
+         * it will instead be set to zero.
+         * 
+         * @param opponent the {@link Character} to be attacked
+         */
+        public String attack(Character opponent) {
 
-    /**
-     * sets the name of the {@link Character} to the specified name
-     * 
-     * @param name the new name of the {@link Character}
-     */
-    public void setName(String name) {
-        this.name = name;
-    }
+            int damage = getAttack() - opponent.getDefense();
+            opponent.health -= damage;
 
-    /**
-     * sets the description of the {@link Character} to the given String
-     * 
-     * @param description the new description of the {@link Character}
-     */
-    public void setDescription(String description) {
-        this.description = description;
+            if (opponent.health < 0 || opponent.health == 0) {
+                opponent.health = 0;
+                return opponent.getName() + " has been defeated by " + getName();
+            }
+
+            else {
+                return getName() + " attacked " + opponent.getName() + " for " + damage;
+            }
+
+        }
+
+        /**
+         * fetches the name of the {@link Character}
+         * 
+         * @return name of the {@link Character}
+         */
+        public String getName() {
+            return this.name;
+        }
+
+        /**
+         * sets the name of the {@link Character} to the specified name
+         * 
+         * @param name the new name of the {@link Character}
+         */
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        /**
+         * sets the description of the {@link Character} to the given String
+         * 
+         * @param description the new description of the {@link Character}
+         */
+        public void setDescription(String description) {
+            this.description = description;
+        }
+
+        public boolean isAlive() {
+            return this.health > 0;
+        }
+
+        public int getHealth() {
+            return this.health;
+        }
+
+        public int getMaxHP() {
+            return this.maxHP;
+        }
+
+        public int getAttack() {
+            return this.attack;
+        }
+        
+        public int getDefense() {
+            return this.defense;
+        }
+
     }
-}
--- src/main/java/edu/rit/swen262/domain/PlayerCharacter.java
+++ src/main/java/edu/rit/swen262/domain/PlayerCharacter.java
@@ -1,106 +1,106 @@
-package edu.rit.swen262.domain;
-
-/**
- * represents the Player Character traveling through the dungeon, extends the
- * {@link Character}
- */
-public class PlayerCharacter extends Character {
-    Armor armor;
-    Weapon weapon;
-    Inventory inventory;
-    Gold gold;
+    package edu.rit.swen262.domain;
 
     /**
-     * initializes a PlayerCharacter with the given name and description. Their
-     * maxHP, attack, and defense stats are set to 100, 10, and 0 respectively by
-     * default.
-     * 
-     * @param name        the given name of the player
-     * @param description the given brief description of the player
-     * @param armor       the equipped armor
-     * @param weapon      the equipped weapon
-     * @param gold        the gold
+     * represents the Player Character traveling through the dungeon, extends the
+     * {@link Character}
      */
+    public class PlayerCharacter extends Character {
+        Armor armor;
+        Weapon weapon;
+        Inventory inventory;
+        Gold gold;
 
-    public PlayerCharacter(String name, String description) {
-        super(name, description, 100, 10, 0);
-        this.armor = null;
-        this.weapon = null;
-        this.inventory = new Inventory(6);
-        this.gold = new Gold(0);
-    }
+        /**
+         * initializes a PlayerCharacter with the given name and description. Their
+         * maxHP, attack, and defense stats are set to 100, 10, and 0 respectively by
+         * default.
+         * 
+         * @param name        the given name of the player
+         * @param description the given brief description of the player
+         * @param armor       the equipped armor
+         * @param weapon      the equipped weapon
+         * @param gold        the gold
+         */
 
-    public PlayerCharacter(String name, String description, Armor armor, Weapon weapon, Inventory inventory,
-            Gold gold) {
-        super(name, description, 100, 10, 0);
-        this.armor = armor;
-        this.weapon = weapon;
-        this.inventory = inventory;
-        this.gold = gold;
-    }
-
-    public String eatFood(Food food) {
-        this.health += food.getHp();
-        if (this.health > this.maxHP) {
-            this.health = this.maxHP;
+        public PlayerCharacter(String name, String description) {
+            super(name, description, 100, 10, 0);
+            this.armor = null;
+            this.weapon = null;
+            this.inventory = new Inventory(6);
+            this.gold = new Gold(0);
         }
 
-        return "Ate food: " + food.getName();
-    }
-
-    public String equipArmor(Armor armor) {
-        if (this.armor == null) {
+        public PlayerCharacter(String name, String description, Armor armor, Weapon weapon, Inventory inventory,
+                Gold gold) {
+            super(name, description, 100, 10, 0);
             this.armor = armor;
-            this.defense += armor.getDefense();
-        }
-
-        else {
-            Armor temp = this.armor;
-            this.defense -= temp.getDefense();
-            this.armor = armor;
-            this.defense += armor.getDefense();
-            for (Bag bag : inventory.getBags()) {
-                boolean result = bag.addItem(temp);
-                if (result) {
-                    break;
-                }
-            }
-        }
-
-        return "Now wearing: " + armor.getName();
-    }
-
-    public String equipWeapon(Weapon weapon) {
-        if (this.weapon == null) {
             this.weapon = weapon;
-            this.attack += weapon.getAttack();
+            this.inventory = inventory;
+            this.gold = gold;
         }
 
-        else {
-            Weapon temp = this.weapon;
-            this.attack -= temp.getAttack();
-            this.weapon = weapon;
-            this.attack += weapon.getAttack();
+        public String eatFood(Food food) {
+            this.health += food.getHp();
+            if (this.health > this.maxHP) {
+                this.health = this.maxHP;
+            }
+
+            return "Ate food: " + food.getName();
+        }
+
+        public String equipArmor(Armor armor) {
+            if (this.armor == null) {
+                this.armor = armor;
+                this.defense += armor.getDefense();
+            }
+
+            else {
+                Armor temp = this.armor;
+                this.defense -= temp.getDefense();
+                this.armor = armor;
+                this.defense += armor.getDefense();
+                for (Bag bag : inventory.getBags()) {
+                    boolean result = bag.addItem(temp);
+                    if (result) {
+                        break;
+                    }
+                }
+            }
+
+            return "Now wearing: " + armor.getName();
+        }
+
+        public String equipWeapon(Weapon weapon) {
+            if (this.weapon == null) {
+                this.weapon = weapon;
+                this.attack += weapon.getAttack();
+            }
+
+            else {
+                Weapon temp = this.weapon;
+                this.attack -= temp.getAttack();
+                this.weapon = weapon;
+                this.attack += weapon.getAttack();
+                for (Bag bag : inventory.getBags()) {
+                    boolean result = bag.addItem(temp);
+                    if (result) {
+                        break;
+                    }
+                }
+            }
+
+            return "Now wielding: " + weapon.getName();
+        }
+
+        public Item findItem(Item item) {
             for (Bag bag : inventory.getBags()) {
-                boolean result = bag.addItem(temp);
-                if (result) {
-                    break;
+                for (Item each : bag.getItems()) {
+                    if (each.equals(item)) {
+                        bag.removeItem(each);
+                        return each;
+                    }
                 }
             }
+            return null;
         }
-
-        return "Now wielding: " + weapon.getName();
     }
-
-    public Item findItem(Item item) {
-        for (Bag bag : inventory.getBags()) {
-            for (Item each : bag.getItems()) {
-                if (each.equals(item)) {
-                    bag.removeItem(each);
-                    return each;
-                }
-            }
-        }
-        return null;
-    }
-}
