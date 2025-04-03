b500490931a048070ddde303a921c2a9c36e2649
emanning686 <emanning1030@gmail.com>
2025-04-03 11:49:03 -0400

    enemy clas

---
+++ src/main/java/edu/rit/swen262/domain/Enemy.java
@@ -0,0 +1,26 @@
+package edu.rit.swen262.domain;
+
+import java.util.List;
+
+import edu.rit.swen262.service.GameState;
+
+public class Enemy extends Character {
+    private GameState mediator;
+    private List<Item> loot;
+    // TO-DO: implement attacking
+
+
+    public Enemy(String name, int maxHP, int attack, int defense) {
+        super(name, name + " (HP: " + maxHP + ", Attack: " + attack + ", Defense: " + defense + ")", maxHP, attack, defense);
+    }
+
+    @Override
+    public RenderRepresentation render() {
+        return RenderRepresentation.ENEMY;
+    }
+
+    @Override
+    public String description() {
+        return super.description;
+    }
+}
