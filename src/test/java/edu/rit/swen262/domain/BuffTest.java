7e2c69a2d8eeccee9bf9731e3376ea1408b63b6a
nickf1130 <nfennelly01@gmail.com>
2025-04-08 13:26:29 -0400

    buffs fully working, tests passing

--- src/test/java/edu/rit/swen262/domain/BuffTest.java
+++ src/test/java/edu/rit/swen262/domain/BuffTest.java
@@ -35,30 +35,76 @@ class BuffTest {
     @Test
     public void test_BuffNotEquals() {
         Gold gold = new Gold(10);
-        Buff Buff = new Buff("Buff", "Buff Description", 0, 0, 0, 0, gold);
-        Buff Buff2 = new Buff("Cool Buff", "Buff Description", 0, 0, 0, 0, gold);
+        Buff buff = new Buff("Buff", "Buff Description", 0, 0, 0, 0, gold);
+        Buff buff2 = new Buff("Cool Buff", "Buff Description", 0, 0, 0, 0, gold);
 
-        assertNotEquals(Buff, Buff2);
+        assertNotEquals(buff, buff2);
 
     }
 
     @Test
     public void test_BuffIsItem() {
         Gold gold = new Gold(10);
-        Buff Buff = new Buff("Buff", "Buff Description", 0, 0, 0, 0, gold);
+        Buff buff = new Buff("Buff", "Buff Description", 0, 0, 0, 0, gold);
 
-        assertEquals(Buff instanceof Item, true);
+        assertEquals(buff instanceof Item, true);
 
     }
 
     @Test
     public void test_ApplyBuff() {
         Gold gold = new Gold(10);
-        Buff Buff = new Buff("Buff", "Buff Description", 10, 5, 10, 2, gold);
+        Buff buff = new Buff("Buff", "Buff Description", 10, 5, 10, 2, gold);
         PlayerCharacter player = new PlayerCharacter("name", "desc", null, null, null, null, null);
 
-        player.useBuff(Buff);
+        player.useBuff(buff);
         assertEquals(15, player.getAttack());
+        assertEquals(10, player.getDefense());
+        assertEquals(102, player.getMaxHP());
+        assertEquals(10, buff.getDuration());
+
+    }
+
+    @Test
+    public void test_DecreaseDuration() {
+
+        Gold gold = new Gold(10);
+        Buff buff = new Buff("Buff", "Buff Description", 10, 5, 10, 2, gold);
+        PlayerCharacter player = new PlayerCharacter("name", "desc", null, null, null, null, null);
+
+        player.useBuff(buff);
+        assertEquals(15, player.getAttack());
+        assertEquals(10, player.getDefense());
+        assertEquals(102, player.getMaxHP());
+        assertEquals(10, buff.getDuration());
+
+        player.updateBuffs();
+        assertEquals(15, player.getAttack());
+        assertEquals(10, player.getDefense());
+        assertEquals(102, player.getMaxHP());
+        assertEquals(9, buff.getDuration());
+
+    }
+
+    @Test
+    public void test_RemoveBuffs() {
+
+
+        Gold gold = new Gold(10);
+        Buff buff = new Buff("Buff", "Buff Description", 1, 5, 10, 2, gold);
+        PlayerCharacter player = new PlayerCharacter("name", "desc", null, null, null, null, null);
+
+        player.useBuff(buff);
+        assertEquals(15, player.getAttack());
+        assertEquals(10, player.getDefense());
+        assertEquals(102, player.getMaxHP());
+        assertEquals(1, buff.getDuration());
+
+        player.updateBuffs();
+        assertEquals(10, player.getAttack());
+        assertEquals(0, player.getDefense());
+        assertEquals(100, player.getMaxHP());
+        assertEquals(0, buff.getDuration());
 
     }
 
